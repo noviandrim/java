a679ed16ff9196b9ae78e74e589921bdb5da4584
mustang <mustang@localhost>
2019-06-19 22:32:50 +0700

    new commit

---
+++ NetBeansProjects/Projects/src/projects/Exercise.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projects;
+
+/**
+ *
+ * @author mustang
+ */
+public class Exercise 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        System.out.println("Muhammad Noviandri");
+        System.out.println("Bogor,19 November");
+        System.out.println("Politeknik Negeri Jakarta");
+    }
+}
